5dde27a9dbccca3ceade8aed826082b34c54fd3e
g-yunjh <yunjh1126@naver.com>
2024-08-10 16:06:49 +0900

    initial

---
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -0,0 +1,21 @@
+package hello.hello_spring.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model) {
+        model.addAttribute("data", "spring!!");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String HelloMVC(@RequestParam(value = "name", required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-templates";
+    }
+}
